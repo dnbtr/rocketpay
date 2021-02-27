d3849e2788803b3487f28fffbc24a18facf9907e
dnbtr <daniel.bittar@alumni.usp.br>
2021-02-27 01:10:11 -0300

    fix: accounts_controller

--- rocketpay/lib/rocketpay_web/controllers/accounts_controller.ex
+++ rocketpay/lib/rocketpay_web/controllers/accounts_controller.ex
@@ -1,19 +1,15 @@
 defmodule RocketpayWeb.AccountsController do
   use RocketpayWeb, :controller
 
-  alias Rocketpay.User
+  alias Rocketpay.Account
 
   action_fallback RocketpayWeb.FallbackController
 
   def deposit(conn, params) do
-    with {:ok, %Account{} = user} <- Rocketpay.deposit(params) do
+    with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
       conn
       |> put_status(:ok)
       |> render("create.json", account: account)
+    end
   end
-
-  def withdraw(conn, params) do
-
-  end
-
 end
